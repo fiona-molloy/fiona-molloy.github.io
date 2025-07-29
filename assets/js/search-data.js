017d05ce3f82a909178e3b67930838a4817eb57a
fiona-molloy <40954655+fiona-molloy@users.noreply.github.com>
2025-07-29 13:49:41 +0000

    Deploying to gh-pages from @ fiona-molloy/fiona-molloy.github.io@08bf103825aeefcc412f2beb4bdf15efe72a6b20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,9 +443,9 @@ ninja.data = [{
           description: "Advanced statistical analyses for identifying the neural basis of executive functions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-substance-use",
+          title: 'Substance use',
+          description: "Neurocognitive mechanisms of addiction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
