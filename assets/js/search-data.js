2124b064b43b302a45c98aa205d95b8cfa7b4224
fiona-molloy <40954655+fiona-molloy@users.noreply.github.com>
2025-07-29 02:40:56 +0000

    Deploying to gh-pages from @ fiona-molloy/fiona-molloy.github.io@e957378b3b7acf1f7ee726e36666e84a4355ffda 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click the icon for a pdf of my full CV, or see below for a quick overview of my background.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
